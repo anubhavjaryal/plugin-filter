0f1ef62cc89d64b29f5b724f0e0f516ae6160356
Anubhav Jaryal <anubhavjaryal@outlook.com>
2019-10-23 20:02:28 +0530

    !added

---
+++ Filter/FilterField.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace ISM\Filters;
+
+class FilterField {
+	private $name;
+	private $compare;
+	private $value = null;
+	public function getName() {
+		return $this->name;
+	}
+	public function setName($name) {
+		$this->name = $name;
+	}
+	public function getCompare() {
+		return $this->compare;
+	}
+	public function setCompare($compare) {
+		$this->compare = $compare;
+	}
+	public function getValue() {
+		return $this->value;
+	}
+	public function setValue($value) {
+		$this->value = $value;
+	}
+	public function __construct($name, $compare, $value) {
+		$this->setName ( $name );
+		$this->setCompare ( $compare );
+		$this->setValue ( $value );
+	}
+	public function buildQuery($modelObj) {
+		switch (strtolower ( $this->compare )) {
+			case "in" :
+				if(gettype($this->getValue ()) != 'array'){
+					$this->setValue([$this->getValue ()]);
+				}
+				return $modelObj->whereIn ( $this->getName (), $this->getValue () );
+			case "between" :
+				return $modelObj->whereBetween ( $this->getName (), $this->getValue () );
+			case "notbetween" :
+				return $modelObj->whereNotBetween ( $this->getName (), $this->getValue () );
+			case "like" :
+				return $modelObj->where ( $this->getName (), 'like', "%" . $this->getValue () . "%" );
+			case "llike" :
+				return $modelObj->where ( $this->getName (), 'like', "%" . $this->getValue () );
+			case "rlike" :
+				return $modelObj->where ( $this->getName (), 'like', $this->getValue () . "%" );
+			case "xlike" :
+        return $modelObj->where ( $this->getName (), 'like', $this->getValue ()  );
+      case "with" :
+        return $modelObj->with($this->getName ());
+			case "=" :
+			case "!=" :
+			case ">" :
+			case ">=" :
+			case "<" :
+			case "<=" :
+				return $modelObj->where ( $this->name, $this->compare, $this->value );
+			default :
+				return $modelObj;
+		}
+	}
+}
---
+++ Filter/FilterTable.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace ISM\Filters;
+
+class FilterTable {
+	private $model;
+	private $fields;
+	public function __construct(FilterModel $model, array $params) {
+		$this->setModel ( $model );
+		$filteredParams = $this->buildFilterParams ( $params, $this->getModel ()->getFilterFormat () );
+		$fields = $this->createFieldData ( $filteredParams );
+		$this->setFields ( $fields );
+	}
+	public function getModel() {
+		return $this->model;
+	}
+	public function setModel( $model) {
+		$this->model = $model;
+	}
+	public function getFields() {
+		return $this->fields;
+	}
+	public function setFields(array $fields) {
+		$this->fields = [ ];
+		foreach ( $fields as $field ) {
+			$this->setField ( $field );
+		}
+	}
+	private function setField(FilterField $field) {
+		$this->fields [] = $field;
+	}
+	public function filterGet($paginationParams=[], $orderBy= null, $select = null) {
+		$paginationParams = Util::getPaginationParams($paginationParams);
+		$query = $this->addFilterFields ();
+		return Util::getPaginationResult ( $query, $paginationParams [Util::PAGE_NO],$paginationParams [Util::PER_PAGE], $orderBy, true, $select );
+	}
+	private function addFilterFields() {
+		$query = $this->model;
+		foreach ( $this->fields as $field ) {
+			$query = $field->buildQuery ( $query );
+		}
+		return $query;
+	}
+	private function createFieldData(array $fieldArray) {
+		$array = [ ];
+		foreach ( $fieldArray as $field ) {
+			$array [] = new FilterField ( $field [0], $field [1], $field [2] );
+		}
+		return $array;
+	}
+	private function buildFilterParams($params, $format) {
+		$filterParams = [ ];
+		foreach ( $params as $k => $v ) {
+			if (isset ( $format [$k] )) {
+				$filterParams [] = [ 
+						$format [$k] [0],
+						$format [$k] [1],
+						$v 
+				];
+			}
+		}
+		return $filterParams;
+	}
+}
